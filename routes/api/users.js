c02c38f13d4dfe7fac8bf0f3f426a6797d67ef16
YevhenBrovchuk <brovchukyevhen@ukr.net>
2023-12-26 20:32:30 +0200

    fix server start

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import userController from "../../controller/auth-controllers.js"
+import userController from "../../controllers/auth-controllers.js"
 import {validateBody} from "../../decorators/index.js";
 import {authenticate, isEmptyBody,upload} from "../../middlewares/index.js";
 import {userInfoSchema, userUpdateSchema, userNormWaterSchema, userLoginSchema, userRegisterForm} from "../../db/models/User.js";
