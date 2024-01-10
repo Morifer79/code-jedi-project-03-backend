a8469470217bf9c7eeebb54d033d97bdfd4c1597
JuliaSmolianska <i.julia.i1990@gmail.com>
2024-01-10 16:07:18 +0200

    update code

--- routes/api/users_router.js
+++ routes/api/users_router.js
@@ -1,11 +1,11 @@
 import express from "express";
 import userController from "../../controllers/user_controllers.js"
-import {validateBody} from "../../decorators/index.js";
-import {authenticate, isEmptyBody,upload} from "../../middlewares/index.js";
-import {userInfoSchema, 
-  userUpdateSchema, 
-  userNormWaterSchema, 
-  userChangePasswordSchema
+import { validateBody } from "../../decorators/index.js";
+import { authenticate, isEmptyBody, upload } from "../../middlewares/index.js";
+import {
+  userInfoSchema,
+  userUpdateSchema,
+  userNormWaterSchema
 } from "../../db/models/User.js";
 
 const userRouter = express.Router();
@@ -14,11 +14,9 @@ userRouter.patch("/avatar", authenticate, upload.single("avatarURL"), userContro
 
 userRouter.get("/:userId", authenticate, validateBody(userInfoSchema), userController.currentUser);
 
-userRouter.patch("/:userId", authenticate,isEmptyBody, validateBody(userUpdateSchema), userController.updateUser);
+userRouter.patch("/:userId", authenticate, isEmptyBody, validateBody(userUpdateSchema), userController.updateUser);
 
 userRouter.patch("/water-rate", authenticate, isEmptyBody, validateBody(userNormWaterSchema), userController.updateWaterNorm);
-
-userRouter.patch("/:userId/change-password", authenticate, isEmptyBody, validateBody(userChangePasswordSchema), userController.changePassword);
 
 userRouter.post("/forgot-password", isEmptyBody, userController.forgotPassword);
 
